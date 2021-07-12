99c7176bf8aee673b6b4194f42137d4190e2a63d
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-07-12 13:10:12 +1000

    Deleted develop fodler

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 app.use(express.urlencoded({ extended: true }));
-app.listen(express.json()); 
+app.use(express.json()); 
 
 app.use(logger('dev'));
 
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker-jb', { 
     useNewUrlParser: true, 
     useFindandModify: false, 
     useUnifiedTopology: true, 
